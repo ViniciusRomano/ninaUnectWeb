5537932a11eccfce9cec6e17c7ca69b381caf1de
Rafael Santos <rafaelnsantos1@gmail.com>
2017-05-06 20:19:19 -0300

    pagina de admin

--- hello/admin.py
+++ hello/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Funcionario
+from .models import Permanencia
+
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ('ra', 'nome', 'rfid')
+
+class PermanenciaAdmin(admin.ModelAdmin):
+    list_display = ('funcionario', 'entrada', 'saida')
 
 # Register your models here.
+admin.site.register(Funcionario, FuncionarioAdmin)
+admin.site.register(Permanencia, PermanenciaAdmin)
