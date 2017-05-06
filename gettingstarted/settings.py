75f7d3c9d8ea3812058a9310fef1a355a46600e0
Rafael Santos <rafaelnsantos1@gmail.com>
2017-05-06 20:13:37 -0300

    config postgresql

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'gettingstarted.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': '192.168.99.100',
+        'PORT': '',
     }
 }
 
