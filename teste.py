8ded589aaf1c93f26d92caa47f08380dd50e0375
Rafael Santos <rafaelnsantos1@gmail.com>
2017-05-10 14:18:16 -0300

    teste para desktop

---
+++ teste.py
@@ -0,0 +1,10 @@
+import django, os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
+django.setup()
+
+from hello.models import Funcionario
+
+try:
+    Funcionario.objects.get(ra=1430581).marcar_saida()
+except Funcionario.DoesNotExist:
+    print ("Funcionario nao encontrado")
