d9905b0962f3a5bc265673e9c03beacaf15269d1
Rafael Santos <rafaelnsantos1@gmail.com>
2017-05-15 14:56:14 -0300

    add departamento

--- hello/admin.py
+++ hello/admin.py
@@ -1,14 +1,22 @@
 from django.contrib import admin
 from .models import Funcionario
 from .models import Permanencia
+from .models import Departamento
 
 class FuncionarioAdmin(admin.ModelAdmin):
-    list_display = ('ra', 'nome', 'rfid')
+    list_display = ('ra', 'nome', 'departamento','rfid')
+    list_filter = ('departamento',)
+    search_fields = ('ra', 'nome')
 
 class PermanenciaAdmin(admin.ModelAdmin):
-    list_display = ('funcionario', 'entrada', 'saida')
-    list_filter = ('funcionario', 'entrada')
+    list_display = ('funcionario', 'entrada', 'saida', 'reposicao')
+    list_filter = ('entrada', 'reposicao')
+    search_fields = ('funcionario__nome', 'funcionario__ra', 'entrada')
+
+class DepartamentoAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
 
 # Register your models here.
-admin.site.register(Funcionario, FuncionarioAdmin)
 admin.site.register(Permanencia, PermanenciaAdmin)
+admin.site.register(Departamento, DepartamentoAdmin)
+admin.site.register(Funcionario, FuncionarioAdmin)
--- hello/models.py
+++ hello/models.py
@@ -5,33 +5,47 @@ from django.utils import timezone
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
 
+class Departamento(models.Model):
+    nome = models.CharField(max_length=150)
+    def __unicode__(self):
+        return self.nome
+
 class Funcionario(models.Model):
     ra = models.PositiveIntegerField(primary_key=True)
     nome = models.CharField(max_length=150)
     rfid = models.CharField(max_length=100)
+    departamento = models.ForeignKey(Departamento)
     
     def marcar_entrada(self):
-        "cria uma nova permanencia se a ultima foi fechada"
+        """
+        cria uma nova permanencia se a ultima foi fechada
+        """
         try:
             p = Permanencia.objects.filter(funcionario=self).latest('entrada') #pega ultima permanencia criada pelo funcionario
             if p.saida != None: #se existir uma saida
                 Permanencia(funcionario=self).save() #cria nova permanencia
             else:
-                print ("Certifique-se que sua ultima permanencia possui uma saida")
+                return "Certifique-se que sua ultima permanencia possui uma saida"
         except Permanencia.DoesNotExist: #primeira permanencia
             Permanencia(funcionario=self).save()
+
+        return "Entrada realizada"
         
     def marcar_saida(self):
-        "escreve horario de saida na ultima permanencia, se a mesma nao possui horario de saida"
+        """
+        escreve horario de saida na ultima permanencia, se a mesma nao possui horario de saida
+        """
         try:
             p = Permanencia.objects.filter(funcionario=self).latest('entrada')
             if p.saida == None:
                 p.saida = timezone.now()
                 p.save()
             else:
-                print ("Faca uma entrada primeiro")
+                return "Faca uma entrada primeiro"
         except Permanencia.DoesNotExist:
-            print ("Nao existem permanencias")
+            return "Nao existem permanencias"
+            
+        return "Saida realizada"
 
     def __unicode__(self):
         return self.nome
@@ -40,3 +54,4 @@ class Permanencia(models.Model):
     funcionario = models.ForeignKey(Funcionario)
     entrada = models.DateTimeField('entrada', auto_now_add=True)
     saida = models.DateTimeField('saida', blank=True, null=True)
+    reposicao = models.BooleanField(default=False)
